5705f3a4e87d67da46f7957e028cfe2a43e77bb3
Cristiano De Michele <cristiano.demichele@gmail.com>
2026-07-09 13:34:02 +0200

    upd

--- MLCG_Framework/tutorials/tel22/fix_priors_wca.py
+++ MLCG_Framework/tutorials/tel22/fix_priors_wca.py
@@ -4,7 +4,7 @@ with open("cg_priors.json", "r") as f:
     data = json.load(f)
 
 if "wca" in data:
-    data["wca"]["sigma"] = 0.6
+    data["wca"]["sigma"] = 0.3
 
 with open("cg_priors.json", "w") as f:
     json.dump(data, f, indent=4)
