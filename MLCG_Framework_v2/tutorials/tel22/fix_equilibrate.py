6974419309ceeba7f8734d64f7b2fc31c80489a1
Cristiano De Michele <cristiano.demichele@gmail.com>
2026-08-06 22:25:45 +0200

    fixes committed

---
+++ MLCG_Framework_v2/tutorials/tel22/fix_equilibrate.py
@@ -0,0 +1,9 @@
+import espressomd
+import numpy as np
+import argparse
+
+import sys
+sys.path.append("../../simulation")
+import run_cg_md
+
+# We'll just run a short equilibration script similar to equilibrate.py
