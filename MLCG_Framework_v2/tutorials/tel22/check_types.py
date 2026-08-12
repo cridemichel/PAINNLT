3c60eda203d18cc5fac2dda5e013cb555fae196c
Cristiano De Michele <cristiano.demichele@gmail.com>
2026-08-12 16:01:12 +0200

    tentativo di fix del training

--- MLCG_Framework_v2/tutorials/tel22/check_types.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-
-dataset = torch.load("cg_dataset.bin")
-types = set()
-for d in dataset:
-    types.update(d['z'].numpy().tolist())
-print(f"Types in dataset: {types}")
--- MLCG_Framework_v2/tutorials/tel22/check_vel.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-chk = np.load('equilibrated.npz')
-vel = chk['v']
-e_kin = 0
-for i in range(len(vel)):
-    has_omega = sum(chk['omega'][i]**2) > 0
-    if has_omega:
-        e_kin += 0.5 * 250 * sum(vel[i]**2)
-print("Calculated E_kin:", e_kin)
