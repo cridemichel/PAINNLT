fb8c460ebb72fa71b1c6de03dadc3dbcfcbda528
Cristiano De Michele <cristiano.demichele@gmail.com>
2026-08-12 16:01:12 +0200

    tentativo di fix del training

--- MLCG_Framework_v2/tutorials/tel22/check_distances.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Load checkpoint
-chk = np.load("tel22_system.npz")
-pos = chk["pos"]
-
-# Wait, the checkpoint only has `pos`, but no `mol_id`. 
-# We don't have the topology here.
--- MLCG_Framework_v2/tutorials/tel22/fix_equilibrate.py
+++
@@ -1,9 +0,0 @@
-import espressomd
-import numpy as np
-import argparse
-
-import sys
-sys.path.append("../../simulation")
-import run_cg_md
-
-# We'll just run a short equilibration script similar to equilibrate.py
